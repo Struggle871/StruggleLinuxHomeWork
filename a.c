bc7f0c249f164b8d4d32c76e7981c22d9387b389
Struggle871 <3375288629@qq.com>
2025-11-28 18:40:32 +0800

    second change

--- a.c
+++ a.c
@@ -2,8 +2,13 @@
 int main(){
 
 
-printf("hello,world\n");
 
+printf("hello,world\n");
+printf("hello,world\n");
+printf("hello,world\n");
+printf("hello,world\n");
+printf("hello,world\n");
+printf("hello,world\n");
 return 0;
 
 }
